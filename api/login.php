05476e68c224afa0f30860d85429343154b0c8ea
Lucas <lukspaiva@hotmail.com>
2017-04-25 22:34:52 -0300

    provider de login

---
+++ api/login.php
@@ -0,0 +1,45 @@
+<?php
+require 'mySQL.php';
+?>
+<?php
+$the_request = &$_POST;
+$postdata = file_get_contents("php://input");
+
+if (isset($postdata)) {
+    $request = json_decode($postdata);
+
+    //recebe variaveis dos usuario coren e senha
+    $login = $request->coren;
+    $senha = $request->senha;
+
+    //cria objeto usado para enviar resposta para usuario
+    $reposta = new stdObject();
+
+
+    //pesquisa pelo coren no bando de dados
+    $sql = "SELECT * FROM auth_user WHERE username = '$login'";
+    $result = $con->query($sql);
+
+    //veirifica se o coren esta cadastrado
+    if ($result->num_rows == 1) {
+        $usuario = $result->fetch_assoc();
+        //veirifca se a senha do usuario obtido é a mesma senha enviada pelo usuario
+        if ($usuario['password'] == $senha) {
+            $reposta->mensagem = "sucesso";
+            $reposta->usuario = $usuario;
+            echo json_encode($reposta);
+        //se a senha estiver incorreta, retorna mensagem dizendo o que estava errado
+        } else {
+            $reposta->mensagem = "senha";
+            $reposta->usuario = "";
+            echo json_encode($reposta);
+        }
+    //se o coren nao existir, retorna mensagem dizendo o que estava errado
+    } else {
+        $reposta->mensagem = "login";
+        $reposta->usuario = "";
+        echo json_encode($reposta);
+    }
+}
+$con->close();
+?>
--- sae/src/providers/login-service.ts
+++ sae/src/providers/login-service.ts
@@ -1,18 +1,41 @@
 import { Injectable } from '@angular/core';
-import { Http } from '@angular/http';
-import 'rxjs/add/operator/map';
+import { Headers, Http, Response } from '@angular/http';
+import { Usuario } from '../../model/usuario';
+import 'rxjs/add/operator/toPromise';
 
-/*
-  Generated class for the LoginService provider.
-
-  See https://angular.io/docs/ts/latest/guide/dependency-injection.html
-  for more info on providers and Angular 2 DI.
-*/
 @Injectable()
 export class LoginService {
 
+  private headers = new Headers({ 'Content-Type': 'application/json' });
+
   constructor(public http: Http) {
-    console.log('Hello LoginService Provider');
+  }
+
+  public logar(usuario: Usuario): Promise<any> {
+    return this.http
+      .post('http://localhost/saeapi/login.php', JSON.stringify(usuario), { headers: this.headers })
+      .toPromise()
+      .then(response => this.extractLoginData(response))
+      .catch(this.tratarError);
+  }
+
+  private extractLoginData(res: Response) {
+    let retorno = { erro: false, mensagem: '', usuario: {} };
+    let data = res.json();
+    if (data) {
+      retorno.mensagem = data.mensagem;
+      if (data == 'sucesso') {
+        retorno.usuario = data.usuario;
+      }
+    } else {
+      retorno.erro = true;
+    }
+    return retorno;
+  }
+
+  private tratarError(error: any) {
+    let retorno = { erro: true, mensagem: JSON.stringify(error) };
+    return retorno;
   }
 
 }
